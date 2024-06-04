c4992ea422d99e946bb93c9523400ac921f50bff
PATRICK.O <patrick.ojeda.91@gmail.com>
2024-06-03 22:13:24 -0400

    [ADD]templates

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from main.views import index
+from main.views import index, about, welcome
 
 urlpatterns = [
     path('', index),
+    path('about/', about),
+    path('welcome/', welcome)
+
 ]
