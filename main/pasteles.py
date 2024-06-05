bb892a933ec07cefc41bbbabd45e81e5c573fc75
PATRICK.O <patrick.ojeda.91@gmail.com>
2024-06-04 20:54:34 -0400

    [ADD] Header logo

---
+++ main/pasteles.py
@@ -0,0 +1,32 @@
+pasteles = [
+  {
+    'id': 1,
+    'nombre': 'Flan Borracho',
+    'descripcion': 'Postre a base de pan frío',
+    'img_url': 'https://dgari.com/wp-content/uploads/2023/05/flan-de-corazon.png',
+    'precio': 3000
+  },
+  
+  {
+    'id': 2,
+    'nombre': '',
+    'descripcion': '',
+    'img_url': '',
+    'precio':  2500
+  },
+  
+  {
+    'id': 3,
+    'nombre': '',
+    'descripcion': '',
+    'img_url': '',
+    'precio': 4000
+  },
+  {
+    'id': 4,
+    'nombre': '',
+    'descripcion': '',
+    'img_url': '',
+    'precio': 2000
+  }
+]
